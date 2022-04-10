63ea828fa47550eae01b89a43707c1e2e994aa0b
tomking23 <tomsenthu2000@gmail.com>
2022-04-10 13:37:06 +0530

    new3

--- tom.java
+++ tom.java
@@ -2,12 +2,7 @@ public class tom
 {
     public static void main(String[] args)
     {
-<<<<<<< HEAD
-        int a=5,b=10;
-        System.out.println("Hello "+(a*b));
-=======
         int a=66,b=10;
-        System.out.println("Hello "+(a+b));
->>>>>>> new1
+        System.out.println("Hello "+(a*b)+"  "+(a+b));
     }
 }
